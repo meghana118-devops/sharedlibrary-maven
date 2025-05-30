13b20a2eb5fe26d0ebd3ee7cf28b0e6b50dc364b
meghana118-devops <c.meghana118@gmail.com>
2025-05-30 14:03:48 -0400

    Create mavenPipeline.groovy

---
+++ vars/mavenPipeline.groovy
@@ -0,0 +1,49 @@
+import com.mylearning.builds.maven
+
+def call(Map pipelineParams) {
+    // an instance of the Calculator class 
+    Mvn Mvn_new = new Mvn(this)
+    pipeline {
+        agent {
+            label 'java-slave'
+        }
+        environment {
+            APPLICATION_NAME = "${pipelineParams.appName}"
+        }
+        tools {
+            maven 'Maven_3.9.0'
+        }
+        stages {
+            stage ('Build') {
+                steps {
+                    echo "Building the project"
+                    echo "************ In Build Stage for ${env.APPLICATION_NAME}  ************"
+                    script {
+                        Mvn_new.run('mvn clean install')
+
+                    }
+                }
+            }
+            stage ('Test') {
+                steps {
+                    echo "Testing the project"
+                }
+            }
+            stage ('DevDeploy') {
+                steps {
+                    echo "Deploying the project to Dev environment"
+                }
+            }
+            stage ('TestDeploy') {
+                steps {
+                    echo "Deploying the project to Test environment"
+                }
+            }
+            stage ('ProdDeploy') {
+                steps {
+                    echo "Deploying the project to Prod environment"
+                }
+            }
+        }
+    }
+}
